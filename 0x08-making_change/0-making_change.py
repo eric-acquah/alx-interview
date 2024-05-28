5dd0049f6961e03a790a90f372c9a6ea44adbc80
Eric Acquah <ericacquah1720@gmail.com>
2024-05-28 10:56:18 +0000

    Making change problem

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+    
+    # Initailize the amount table
+    table = [float('inf')] * (total + 1)
+    table[0] = 0
+
+    # Compute the minimum coins required for each amount
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            table[amount] = min(table[amount], table[amount - coin] + 1)
+
+    if table[total] == float('inf'):
+        return -1
+    return table[total]
