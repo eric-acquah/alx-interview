e0a0d2fd9d429cafc95023d80bdc25206992ceee
Eric Acquah <ericacquah1720@gmail.com>
2024-05-30 09:04:24 +0000

    Island parameter solution

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+This is a function that returns the perimeter of the island described in grid.
+
+"""
+
+def island_perimeter(grid):
+    """
+    Function to return the perimeter of the island described in grid.
+    """
+
+    if not grid or not grid[0]:
+        return 0
+    
+    rows = len(grid)
+    cols = len(grid[0])
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
